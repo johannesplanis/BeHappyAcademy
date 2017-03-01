f94aae1de38205ea97f7e8b93097db74a5681529
Jan Pluta <jan.pluta.jr@gmail.com>
2017-03-01 18:46:15 +0100

    initial commit

--- app/src/main/java/co/planis/behappylesson2/LoginActivity.java
+++ app/src/main/java/co/planis/behappylesson2/LoginActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, ForgotPasswordFragment.OnFragmentInteractionListener {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,21 +12,5 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
         setContentView(R.layout.activity_login);
 
         getSupportFragmentManager().beginTransaction().add(R.id.login_activity_container,new LoginFragment()).commit();
-    }
-
-    @Override
-    public void loggedIn() {
-        startActivity(new Intent(this,MainActivity.class));
-        finish();
-    }
-
-    @Override
-    public void onForgotPasswordClicked() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.login_activity_container,new ForgotPasswordFragment()).addToBackStack("FORGOT").commit();
-    }
-
-    @Override
-    public void passwordSent() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.login_activity_container,new LoginFragment()).commit();
     }
 }
--- app/src/main/java/co/planis/behappylesson2/LoginFragment.java
+++ app/src/main/java/co/planis/behappylesson2/LoginFragment.java
@@ -17,11 +17,7 @@ import butterknife.Unbinder;
 
 public class LoginFragment extends Fragment {
 
-    @BindView(R.id.etEmail)
-    EditText etEmail;
-    @BindView(R.id.etPassword)
-    EditText etPassword;
-    Unbinder unbinder;
+
     private OnFragmentInteractionListener mListener;
 
     public LoginFragment() {
@@ -34,7 +30,6 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
@@ -59,39 +54,16 @@ public class LoginFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unbinder.unbind();
     }
 
     @OnClick(R.id.btnLogin)
     public void onLoginClick() {
-        String message;
 
         String goodEmail = "admin@behappy.io";
         String goodPassword = "alamakota123";
-
-        if (!goodEmail.equals(etEmail.getText().toString())) {
-            message = "Nieprawidłowy adres email";
-            etEmail.setText("");
-            etPassword.setText("");
-        } else if (!goodPassword.equals(etPassword.getText().toString())) {
-            message = "NIeprawidłowe hasło";
-            etPassword.setText("");
-        } else {
-            message = "Zalogowano";
-            mListener.loggedIn();
-        }
-        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
-
-    @OnClick(R.id.tvForgotPassword)
-    public void onForgotPasswordClick() {
-        mListener.onForgotPasswordClicked();
-    }
-
 
     public interface OnFragmentInteractionListener {
         void loggedIn();
-
-        void onForgotPasswordClicked();
     }
 }
